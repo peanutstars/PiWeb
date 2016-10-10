89d96f94c379b5e85ff1c9d96e5df17f1b408b94
peanutstars <peanutstars.lee@gmail.com>
2016-10-10 23:20:29 +0900

    run as a service when install this package.

--- src/piwserver.py
+++ src/piwserver.py
@@ -11,8 +11,13 @@ bootstrap()
 
 # debugging purpose, e.g. run with PyDev debugger
 if __name__ == '__main__':
+    import sys ;
     import cherrypy
-    cherrypy.engine.signals.subscribe()
-    # Daemonizer(cherrypy.engine).subscribe() ;
+
+    if '--daemon' in sys.argv :
+        Daemonizer(cherrypy.engine).subscribe() ;
+    else :
+        cherrypy.engine.signals.subscribe()
+
     cherrypy.engine.start()
     cherrypy.engine.block()
